6b4d3b1ce38a0d6b19021aad68d159cfec51fb3b
Cameron Ryan-Pears <cameron.ryan.pears@gmail.com>
2018-10-31 22:28:04 +1300

    Can login to API

--- app/src/main/java/nz/org/cacophony/sidekick/LoginScreen.kt
+++ app/src/main/java/nz/org/cacophony/sidekick/LoginScreen.kt
@@ -1,12 +1,43 @@
 package nz.org.cacophony.sidekick
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import kotlin.concurrent.thread
 
 class LoginScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+        val apiUrlEditText = findViewById<EditText>(R.id.api_url_input)
+        apiUrlEditText.setText(CacophonyAPI.getServerURL(applicationContext))
+        if (CacophonyAPI.getNameOrEmail(applicationContext) != "") {
+            gotoMainActivity()
+        }
+    }
+
+    fun login(v : View) {
+        thread(start = true) {
+            val nameOrEmailEditText = findViewById<EditText>(R.id.username_email_login)
+            val passwordEditText = findViewById<EditText>(R.id.password_login)
+            val apiUrlEditText = findViewById<EditText>(R.id.api_url_input)
+            if (CacophonyAPI.newUser(applicationContext, nameOrEmailEditText.text.toString(), passwordEditText.text.toString(), apiUrlEditText.text.toString())) {
+                nameOrEmailEditText.post {
+                    nameOrEmailEditText.text.clear()
+                }
+                passwordEditText.post {
+                    passwordEditText.text.clear()
+                }
+                gotoMainActivity()
+            }
+        }
+    }
+
+    private fun gotoMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
