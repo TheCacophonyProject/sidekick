fd86f171157b3536723b368551ea467ad034c371
Cameron Ryan-Pears <cameron.ryan.pears@gmail.com>
2019-01-18 17:45:32 +1300

    Removed commented out code

--- app/src/main/java/nz/org/cacophony/sidekick/SettingsActivity.kt
+++ app/src/main/java/nz/org/cacophony/sidekick/SettingsActivity.kt
@@ -15,15 +15,6 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        /*
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-        */
-
     }
 
     fun logout(v: View) {
