8f53c3c1128d16e43299385c0edfcc99110c0cef
Giampaolo <g.ferraro22@gmail.com>
2019-09-26 16:07:29 +1200

    added firebase and crashlytics

--- app/src/main/java/nz/org/cacophony/sidekick/RecordingDao.java
+++ app/src/main/java/nz/org/cacophony/sidekick/RecordingDao.java
@@ -1,9 +1,8 @@
 package nz.org.cacophony.sidekick;
 
-import android.arch.lifecycle.LiveData;
-import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Insert;
-import android.arch.persistence.room.Query;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
 
 import java.util.List;
 
