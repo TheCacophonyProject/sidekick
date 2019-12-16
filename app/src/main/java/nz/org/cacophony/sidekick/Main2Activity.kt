27650aa8324d5cd53f81b47906e70b2060264258
cam <cameron.ryan.pears@gmail.com>
2019-12-16 22:58:41 +1300

    Change how menu navigation is handled

--- app/src/main/java/nz/org/cacophony/sidekick/Main2Activity.kt
+++ app/src/main/java/nz/org/cacophony/sidekick/Main2Activity.kt
@@ -2,42 +2,39 @@ package nz.org.cacophony.sidekick
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.common.internal.Constants
 import com.google.android.material.navigation.NavigationView
 import nz.org.cacophony.sidekick.ui.devices.DevicesFragment
 import nz.org.cacophony.sidekick.ui.home.HomeFragment
 import nz.org.cacophony.sidekick.ui.recordings.RecordingsFragment
+import nz.org.cacophony.sidekick.ui.settings.SettingsFragment
 
 class Main2Activity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navView: NavigationView
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+        toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_devices, R.id.nav_recordings,
-                R.id.nav_settings), drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setUpNavigationView()
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -46,25 +43,15 @@ class Main2Activity : AppCompatActivity() {
     }
 
     fun openDevicesFragment(v: View) {
-        val ft = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.nav_host_fragment, DevicesFragment())
-        ft.commitAllowingStateLoss()
+        loadFragment(DevicesFragment())
     }
 
     fun openRecordingsFragment(v: View) {
-        val ft = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.nav_host_fragment, RecordingsFragment())
-        ft.commit()
-    }
-
-    private fun openHomeFragment() {
-        val ft = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.nav_host_fragment, HomeFragment())
-        ft.commit()
+        loadFragment(RecordingsFragment())
     }
 
     override fun onBackPressed() {
-        openHomeFragment()
+        loadFragment(HomeFragment())
     }
 
     @Suppress("UNUSED_PARAMETER")
@@ -74,5 +61,43 @@ class Main2Activity : AppCompatActivity() {
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
         startActivity(intent)
         finish()
+    }
+
+    fun loadFragment(f: Fragment) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.nav_host_fragment, f)
+        ft.commit()
+        drawerLayout.closeDrawers()
+    }
+
+
+    private fun setUpNavigationView() {
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.nav_home, R.id.nav_devices, R.id.nav_recordings), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        navView.setNavigationItemSelectedListener(object : NavigationView.OnNavigationItemSelectedListener {
+
+            override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+
+                when (menuItem.itemId) {
+                    R.id.nav_home -> {
+                        loadFragment(HomeFragment())
+                    }
+                    R.id.nav_devices -> {
+                        loadFragment(DevicesFragment())
+                    }
+                    R.id.nav_settings -> {
+                        loadFragment(SettingsFragment())
+                    }
+                    R.id.nav_recordings -> {
+                        loadFragment(RecordingsFragment())
+                    }
+                }
+                toolbar.title = "$menuItem"
+                return true
+            }
+        })
     }
 }
