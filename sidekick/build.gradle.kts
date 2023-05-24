17570a7f0636769d4dc2c2baa07b7fb8d34b726c
abra <patrickb321@gmail.com>
2023-05-24 12:59:07 +1200

    feat: update capacitor 5.0

--- sidekick/build.gradle.kts
+++ sidekick/build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("com.android.tools.build:gradle:8.0.0")
         classpath("com.google.gms:google-services:4.3.15")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.5")
     }
@@ -14,8 +14,8 @@ plugins {
     id("com.android.application").version("7.3.1").apply(false)
     id("com.android.library").version("7.3.1").apply(false)
     kotlin("android").version("1.7.10").apply(false)
-    kotlin("multiplatform").version("1.8.10").apply(false)
-
+    kotlin("multiplatform").version("1.8.21").apply(false)
+    kotlin("native.cocoapods") version "1.8.21"
 }
 
 
--- sidekick/shared/build.gradle.kts
+++ sidekick/shared/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     kotlin("multiplatform")
-    kotlin("native.cocoapods")
+    kotlin("native.cocoapods") version "1.8.21"
     id("com.android.library")
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("plugin.serialization") version "1.8.21"
 }
 
 kotlin {
@@ -13,27 +13,20 @@ kotlin {
             }
         }
     }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
 
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "shared"
-        }
-    }
-
-    cocoapods {
-        summary = "Cacophony Project Shared Module"
-        homepage = "Link to the Shared Module homepage"
-        version = "1.0"
-        ios.deploymentTarget = "13.0"
-        podfile = project.file("../App/Podfile")
-        framework {
-            baseName = "shared"
-        }
-    }
+   cocoapods {
+       summary = "Cacophony Project Shared Module"
+       homepage = "Link to the Shared Module homepage"
+       version = "1.0"
+       ios.deploymentTarget = "13.0"
+       podfile = project.file("../App/Podfile")
+       framework {
+           baseName = "shared"
+       }
+   }
 
     sourceSets {
         val ktorVersion = "2.1.3"
@@ -81,15 +74,7 @@ kotlin {
             dependencies {
                 implementation("io.ktor:ktor-client-darwin:$ktorVersion")
             }
-        }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
+
         }
     }
 }
