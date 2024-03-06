b165c8f8f6a53dcdf7bed72c31a4cddc52b29445
Patrick Baxter <patrickb321@gmail.com>
2024-03-06 16:27:11 +1300

    increment patch ver

--- sidekick/app/build.gradle.kts
+++ sidekick/app/build.gradle.kts
@@ -16,7 +16,7 @@ val sdk = 33
 val minSdkVersion = 22
 val majorVersion = 3
 val minorVersion = 3
-val patchVersion = 4
+val patchVersion = 5
 android {
     namespace = "nz.org.cacophony.sidekick"
     compileSdk = sdk
